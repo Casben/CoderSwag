8e0803a848c59387e9691491ac62b05ebbd4942c
HDodge <herbiedodge@gmail.com>
2021-04-04 00:03:58 -0500

    add tableview datasource and delegate to CategoriesVC

--- CoderSwag/Controller/CategoriesVC.swift
+++ CoderSwag/Controller/CategoriesVC.swift
@@ -13,10 +13,35 @@ class CategoriesVC: UIViewController {
     
     @IBOutlet weak var categoryTable: UITableView!
 
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+    
+    //MARK: - Helpers
+    
+    func configure() {
+        categoryTable.dataSource = self
+        categoryTable.delegate = self
     }
 
 
 }
 
+extension CategoriesVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
+            let category = DataService.instance.getCategories()[indexPath.row]
+            cell.updateViews(category: category)
+            return cell
+        } else {
+            return CategoryCell()
+        }
+    }
+}
--- CoderSwag/View/CategoryCell.swift
+++ CoderSwag/View/CategoryCell.swift
@@ -14,14 +14,9 @@ class CategoryCell: UITableViewCell {
     @IBOutlet weak var categoryImage: UIImageView!
     @IBOutlet weak var categoryTitle: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
+    func updateViews(category: Category) {
+        categoryImage.image = UIImage(named: category.imageName)
+        categoryTitle.text = category.title
     }
 
 }
