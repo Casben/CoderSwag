2310ce658534af124fc88dbe5c8808c5e311a5d6
HDodge <herbiedodge@gmail.com>
2021-04-04 19:01:28 -0500

    add productCell and categoryDetailVC git status

---
+++ CoderSwag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by Herbert Dodge on 4/4/21.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    //MARK: - Properties
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
